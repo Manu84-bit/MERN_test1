335de2c4c59ffb83d56f681e96633be690cac031
Manu84-bit <67926609+Manu84-bit@users.noreply.github.com>
2021-10-07 19:47:27 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const URI =
-  "mongodb+srv://manu84:Servidor2@cluster0.ddwml.mongodb.net/demo_mongodb?retryWrites=true&w=majority";
+  "mongodb+srv://user_name:password@cluster0.ddwml.mongodb.net/demo_mongodb?retryWrites=true&w=majority";
 
 //En la siguiente línea se especifica la conexión a la base de datos local o remota (en este caso es remota en Mongo Atlas)
 mongoose
